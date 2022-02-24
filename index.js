2f00786709dbf501c73535f2047fd69e16562b64
jessica <fuerte.jessica10@gmail.com>
2022-02-23 19:02:09 -0600

    all tests passing

--- index.js
+++ index.js
@@ -97,7 +97,6 @@ class Counter {
    */
   next() {
     const result = this.seasons[this.currentSeason]
-    this.currentSeason++
     if (this.currentSeason === 3) {
       this.currentSeason = 0
     } else {
@@ -117,7 +116,8 @@ class Car {
   constructor(name, tankSize, mpg) {
     this.odometer = 0 // car initilizes with zero miles
     this.tank = tankSize // car initiazes full of gas
-    // ✨ initialize whatever other properties are needed
+    this.tankSize = tankSize
+    this.mpg = mpg
   }
 
   /**
@@ -134,7 +134,15 @@ class Car {
    * focus.drive(200) // returns 600 (ran out of gas after 100 miles)
    */
   drive(distance) {
-    // ✨ implement
+    const milesCanDrive = this.tank * this.mpg
+    if(distance <= milesCanDrive) {
+      this.odometer = this.odometer + distance 
+      this.tank = this.tank - (distance / this.mpg)
+    } else {
+    this.tank = 0
+    this.odometer = this.odometer + milesCanDrive
+    }
+    return this.odometer
   }
 
   /**
@@ -149,7 +157,13 @@ class Car {
    * focus.refuel(99) // returns 600 (tank only holds 20)
    */
   refuel(gallons) {
-    // ✨ implement
+    const gallonsThatFit = this.tankSize - this.tank
+    if (gallons <= gallonsThatFit) {
+      this.tank = this.tank + gallons
+    } else {
+      this.tank = this.tankSize
+    }
+    return this.tank * this.mpg
   }
 }
 
@@ -167,7 +181,10 @@ class Car {
  * })
  */
 function isEvenNumberAsync(number) {
-  // ✨ implement
+  if (typeof number !== 'number' || isNaN(number)) {
+    throw new Error('Invalid number')
+  }
+  return number % 2 === 0 || false
 }
 
 module.exports = {
